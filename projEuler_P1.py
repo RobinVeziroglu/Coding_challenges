46193133ea286a461e6ed56811e8f00d88fbaadb
Veziroglu Robin <Robin.Veziroglu@volvo.com>
2022-02-22 19:24:17 +0100

    Added solution

--- projEuler_P1.py
+++ projEuler_P1.py
@@ -11,3 +11,11 @@ Created on Tue Feb 22 17:41:54 2022
 #The sum of these multiples is 23.
 
 #Find the sum of all the multiples of 3 or 5 below 1000.
+
+def mult_3_5(x):
+    lst=[y for y in range(1,x) if y%3==0 or y%5==0]
+    return "The sum of all the multiples of 3 or 5 below {} is equal to {}.".format(x,sum(lst))
+
+if __name__=="__main__":
+    print(mult_3_5(10))
+    print(mult_3_5(1000))
